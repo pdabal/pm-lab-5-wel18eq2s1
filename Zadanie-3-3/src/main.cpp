63717d81f7833a56e9d0c50dcd05d049549bb331
„Kacper <kacper.janowski@student.wat.edu.pl>
2020-06-22 09:33:09 +0200

    Wykonano zadanie 3.3.2 – tekst pływający

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -3,14 +3,37 @@
 #include <Wire.h>
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+char tablica[] = "Kacper";
+int tabLen = 0;
 
-void setup() {
+void lcdClear(void);
+
+void setup(){
 	lcd.begin(16, 2);
-	lcd.print("Janowski");
+	lcd.print("Kacper Janowski");
+	tabLen = sizeof(tablica);
+	delay(2000);
 }
 
 void loop(){
+	for (int i = 0; i < (18 - tabLen); i++){
+		lcdClear();
+		lcd.setCursor(i, 0);
+		lcd.print(tablica);
+		delay(1000);
+	}
+	
+	for (int i = (17 - tabLen); i >= 0; i--){
+		lcdClear();
+		lcd.setCursor(i, 1);
+		lcd.print(tablica);
+		delay(1000);
+	}
+}
+
+void lcdClear(void){
+	lcd.setCursor(0, 0);
+	lcd.print("................");
 	lcd.setCursor(0, 1);
-	lcd.print(millis() / 1000);
-	delay(500);
+	lcd.print("................");
 }
