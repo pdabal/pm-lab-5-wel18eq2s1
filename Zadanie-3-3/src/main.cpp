c0ec15b12453ba4bf39a2f43b0d9b74ed65c7a5f
Jakub_Poręba <jakub.poreba@student.wat.edu.pl>
2020-06-22 09:29:18 +0200

    Wykonano zadanie 3.3.2 – tekst pływający

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -2,14 +2,37 @@
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+char tablica[] = "Jakub";
+int tabLen = 0;
 
-void setup() {
+void lcdClear(void);
+
+void setup(){
 	lcd.begin(16, 2);
-	lcd.print("Poreba");
+	lcd.print("Jakub Poreba");
+	tabLen = sizeof(tablica);
+	delay(2000);
 }
 
 void loop(){
+	for (int i = 0; i< (18 - tabLen); i++){
+		lcdClear();
+		lcd.setCursor(i, 0);
+		lcd.print(tablica);
+		delay(1000);
+	}
+	
+	for (int i = (17 - tabLen); i >= 0; i--){
+		lcdClear();
+		lcd.setCursor(i, 1);
+		lcd.print(tablica);
+		delay(1000);
+	}
+}
+
+void lcdClear(void){
+	lcd.setCursor(0, 0);
+	lcd.print("................");
 	lcd.setCursor(0, 1);
-	lcd.print(millis() / 1000);
-	delay(500);
+	lcd.print("................");
 }
